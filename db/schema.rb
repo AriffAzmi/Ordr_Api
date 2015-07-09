39c9cf59b8a5f8e4fba91f764324e7ff75f7515d
Muhammad Ariff Bin Noor Azmi <ariff.azmi16@gmail.com>
2015-07-09 06:27:57 +0000

    Create table drinks in database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150707034316) do
+ActiveRecord::Schema.define(version: 20150709062428) do
+
+  create_table "drinks", force: :cascade do |t|
+    t.text     "drinks"
+    t.integer  "qty"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "food_lists", force: :cascade do |t|
     t.text     "food"
