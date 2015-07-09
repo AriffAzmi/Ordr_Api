d1346fa3fec9fe37218b8ad9da3eebbe0a975272
Muhammad Ariff Bin Noor Azmi <ariff.azmi16@gmail.com>
2015-07-09 06:38:10 +0000

    Add method to create drink and render the data as json to pass to Api

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -1,2 +1,38 @@
 class DrinksController < ApplicationController
+    
+    def new
+        @drink = Drink.new
+    end
+    
+    def index
+        @drinks = Drink.all
+        
+        respond_to do |format|
+            format.html { render 'index' }
+            format.json { render json: @drinks}
+        end
+    end
+    
+    def create
+        
+        @drink = drink.new(drink_params)
+        if @drink.save
+        flash[:alert] = "Successfully create drink"
+        redirect_to @drink
+        else
+            render 'new'
+        end        
+    end
+    
+    def show
+        @drink = Drink.all
+    end
+    
+    
+    private
+    
+    def drink_params
+        params.require(:drink).permit(:drinks, :qty)
+    end
+    
 end
