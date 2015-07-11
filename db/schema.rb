7195275f078e5e416d3c773b58fc648136014aa6
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2015-07-11 15:41:53 +0800

    Add role to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150710164006) do
+ActiveRecord::Schema.define(version: 20150711073724) do
 
   create_table "drinks", force: :cascade do |t|
     t.text     "name"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20150710164006) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "role"
   end
 
 end
